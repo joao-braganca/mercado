b17d245bf9b2e9506b93f24289bea8d256a9ed47
João Victor Dias <90797475+joao-braganca@users.noreply.github.com>
2025-03-07 11:02:00 -0300

    Atualizar o login.php

--- login.php
+++ login.php
@@ -1,4 +1,19 @@
 <?php
+
+/* BANCO DE DADOS */
+$localhost = "localhost";
+$banco = "mercado";
+$user_banco = "root";
+$password = "Root";
+
+$mysqli = new mysqli($localhost, $user_banco, $password, $banco);
+if($mysqli->connect-error){
+acho "falhou algo aí meu bom, olha só 
+}
+
+/* CODIGO LOGIN */
+
+
 $usuario=$_POST["usuario"];
 $senha=$_POST["senha"];
 
