a149a6f9757cf9fa470a65076068e953b1dcf21f
joao-braganca <jvdbraganca@gmail.com>
2025-03-08 13:41:03 -0300

    correcao php e mysql

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 
 /* BANCO DE DADOS */
-$localhost = "localhost";
+$localhost = "127.0.0.1";
 $banco = "mercado";
 $user_banco = "root";
-$password = "Root";
+$password = "";
 
 $mysqli = new mysqli($localhost, $user_banco, $password, $banco);
-if($mysqli->connect-error){
-acho "falhou algo aí meu bom, olha só: " . $mysqli->connect-error . " .";
+if($mysqli->connect_error){
+echo "falhou algo aí meu bom, olha só: " . $mysqli->connect_error . " .";
 }
 
 /* CODIGO LOGIN */
